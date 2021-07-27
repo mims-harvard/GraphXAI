e170199850b4d47c0d2702e8b54f312c28d39c6c
owencqueen <owencqueen@hotmail.com>
2021-07-27 14:49:39 -0400

    Subgraphx, visualizations changed, modifications to test scripts

--- test_scripts/test_CAM_BA.py
+++ test_scripts/test_CAM_BA.py
@@ -6,15 +6,14 @@ from graphxai.explainers import CAM, Grad_CAM
 from graphxai.explainers.utils.visualizations import visualize_subgraph_explanation
 from graphxai.gnn_models.node_classification import BA_Houses, GCN, train, test
 
-
 n = 300
 m = 2
 num_houses = 20
 
 bah = BA_Houses(n, m)
-data, inhouse = bah.get_data(num_houses)
+data, inhouse = bah.get_data(num_houses, multiple_features=True)
 
-model = GCN(64, input_feat = 1, classes = 2)
+model = GCN(64, input_feat = 3, classes = 2)
 print(model)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
@@ -29,6 +28,7 @@ node_idx = random.choice(inhouse)
 
 model.eval()
 pred = model(data.x, data.edge_index)[node_idx, :].reshape(-1, 1)
+pred_class = pred.argmax(dim=0).item()
 print('pred shape', pred.shape)
 
 print('GROUND TRUTH LABEL: \t {}'.format(data.y[node_idx].item()))
@@ -37,28 +37,30 @@ print('PREDICTED LABEL   : \t {}'.format(pred.argmax(dim=0).item()))
 act = lambda x: torch.argmax(x, dim=1)
 cam = CAM(model, '', activation = act)
 
-exp, khop_info = cam.get_explanation_node(data.x, node_idx = int(node_idx), label = data.y[node_idx], edge_index = data.edge_index)
+exp, khop_info = cam.get_explanation_node(data.x, node_idx = int(node_idx), label = pred_class, edge_index = data.edge_index)
 subgraph_eidx = khop_info[1]
 
-#visualize_categorical_graph(data, show = True)
+y_subgraph = data.y[khop_info[0]]
+trim_exp = exp['feature'][khop_info[0]]
+print('CAM trim_exp', trim_exp)
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
 # Ground truth plot:
-#gt = [khop_info[0][i] for i in len(khop_info[0].tolist())]
-print('Number house nodes:', sum(data.y.tolist()))
-visualize_subgraph_explanation(subgraph_eidx, data.y.tolist(), node_idx = int(node_idx), ax = ax1, show = False)
+visualize_subgraph_explanation(subgraph_eidx, y_subgraph, node_idx = int(node_idx), ax = ax1, show = False)
 ax1.set_title('Ground Truth')
 
 # CAM plot:
-visualize_subgraph_explanation(subgraph_eidx, exp['feature'], node_idx = int(node_idx), ax = ax2, show = False)
+visualize_subgraph_explanation(subgraph_eidx, trim_exp, node_idx = int(node_idx), ax = ax2, show = False)
 ax2.set_title('CAM')
 
 gcam = Grad_CAM(model, '', criterion = criterion)
 exp, khop_info = gcam.get_explanation_node(data.x, data.y, int(node_idx), data.edge_index, average_variant=False)
 
+trim_exp = exp['feature'][khop_info[0]]
+
 #Grad-CAM plot:
-visualize_subgraph_explanation(subgraph_eidx, exp['feature'], node_idx = int(node_idx), ax = ax3, show = False)
+visualize_subgraph_explanation(subgraph_eidx, trim_exp, node_idx = int(node_idx), ax = ax3, show = False)
 ax3.set_title('Grad-CAM')
 
 ymin, ymax = ax1.get_ylim()
