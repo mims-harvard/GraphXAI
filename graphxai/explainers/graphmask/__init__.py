b79147dfb88927df73f4574b9ec3ff4a4891e282
Emmy Chen <emmyywchen@gmail.com>
2021-08-29 19:42:51 -0400

    Add GraphMASK

--- graphxai/explainers/graphmask/__init__.py
+++ graphxai/explainers/graphmask/__init__.py
@@ -1,1 +1,8 @@
-from .graphmask import GraphMASK
+"""
+Code is based on https://github.com/MichSchli/GraphMask.
+"""
+
+from .lagrangian_optimization import LagrangianOptimization
+from .hard_concrete import HardConcrete
+from .multiple_inputs_layernorm_linear import MultipleInputsLayernormLinear
+from .squeezer import Squeezer
