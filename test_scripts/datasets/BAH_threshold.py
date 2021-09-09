b2abd36d0aac66c9e4cffb731d4034c153b270c4
Owen Queen <owencqueen@hotmail.com>
2021-09-09 10:48:12 -0400

    Thresholding experiment added

--- test_scripts/datasets/BAH_threshold.py
+++ test_scripts/datasets/BAH_threshold.py
@@ -1,13 +1,15 @@
+import sys
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+import matplotlib.pyplot as plt
+from torch_geometric.nn import GCNConv, GINConv
 
 from graphxai.datasets.ba_houses import BAHouses as BAH
 #from graphxai.gnn_models.node_classification import train, test
 
 class GCN_1layer(torch.nn.Module):
     def __init__(self, hidden_channels, input_feat, classes):
-        super(GCN_2layer, self).__init__()
+        super(GCN_1layer, self).__init__()
         self.conv1 = GCNConv(input_feat, hidden_channels)
 
     def forward(self, x, edge_index):
@@ -25,6 +27,40 @@ class GCN_2layer(torch.nn.Module):
         x = x.relu()
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.conv2(x, edge_index)
+        return x
+
+class GIN_2layer(torch.nn.Module):
+    def __init__(self, hidden_channels, input_feat, classes):
+        super(GIN_2layer, self).__init__()
+        self.mlp_gin1 = torch.nn.Linear(input_feat, hidden_channels)
+        self.gin1 = GINConv(self.mlp_gin1)
+        self.mlp_gin2 = torch.nn.Linear(hidden_channels, classes)
+        self.gin2 = GINConv(self.mlp_gin2)
+
+    def forward(self, x, edge_index):
+        x = self.gin1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.gin2(x, edge_index)
+        return x
+
+class GIN_3layer(torch.nn.Module):
+    def __init__(self, hidden_channels, input_feat, classes):
+        super(GIN_3layer, self).__init__()
+        self.mlp_gin1 = torch.nn.Linear(input_feat, hidden_channels)
+        self.gin1 = GINConv(self.mlp_gin1)
+        self.mlp_gin2 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.gin2 = GINConv(self.mlp_gin2)
+        self.mlp_gin3 = torch.nn.Linear(hidden_channels, classes)
+        self.gin3 = GINConv(self.mlp_gin3)
+
+    def forward(self, x, edge_index):
+        x = self.gin1(x, edge_index)
+        x = x.relu()
+        x = self.gin2(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.gin3(x, edge_index)
         return x
 
 def train(model, optimizer,
@@ -54,23 +90,30 @@ class BAH_opts:
     k = 1
     seed = None
     plant_method = 'local'
+    in_hood_numbering = True
+    threshold = 3
 
 params = BAH_opts()
 
 bah = BAH(
-        num_hops = 2, 
+        num_hops = 3, 
         n = params.n,
         m = params.m,
         k = params.k,
         seed = params.seed,
         plant_method = params.plant_method,
-        in_hood_numbering = True)
+        in_hood_numbering = params.in_hood_numbering,
+        threshold = params.threshold)
 
 data = bah.get_graph()
-num_classes = data.y.unique().max().item()
-print(num_classes)
+num_classes = 2
 
-model = GCN_2layer(128, input_feat=3, classes=num_classes + 1)
+# plt.hist(data.y.tolist())
+# plt.title('Distribution of node labels')
+# plt.show()
+# exit()
+
+model = GIN_3layer(128, input_feat=3, classes=num_classes)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 criterion = torch.nn.CrossEntropyLoss()
