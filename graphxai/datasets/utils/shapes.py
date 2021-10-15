e18d3df2aa52c66b7d743ee880ab22e82ac04fe7
owencqueen <owencqueen@hotmail.com>
2021-10-14 20:50:13 -0400

    Resolve shape conflict

--- graphxai/datasets/utils/shapes.py
+++ graphxai/datasets/utils/shapes.py
@@ -1,24 +1,50 @@
+import random
+import torch
 import networkx as nx
 
-def get_house():
-    '''
-    Defines house in terms of a nx graph.
-    '''
-    G = nx.Graph() # Different from G
-    nodes = list(range(5))
-    G.add_nodes_from(nodes)
+# def get_house():
+#     '''
+#     Defines house in terms of a nx graph.
+#     '''
+#     G = nx.Graph() # Different from G
+#     nodes = list(range(5))
+#     G.add_nodes_from(nodes)
 
-    # Define full cycle:
-    connections = [(nodes[i], nodes[i+1]) for i in nodes[:-1]]
-    connections += [(nodes[-1], nodes[0])]
-    connections += [(1, 4)] # Cross-house
+#     # Define full cycle:
+#     connections = [(nodes[i], nodes[i+1]) for i in nodes[:-1]]
+#     connections += [(nodes[-1], nodes[0])]
+#     connections += [(1, 4)] # Cross-house
 
-    G.add_edges_from(connections)
-    return G
+#     G.add_edges_from(connections)
+#     return G
 
 def get_flag():
     pass
 
 # Set common shapes:
-house = get_house()
+house = nx.house_graph()
+house_x = nx.house_x_graph()
+diamond = nx.diamond_graph()
+pentagon = nx.cycle_graph(n=5)
+wheel = nx.wheel_graph(n=6)
+star = nx.star_graph(n=5)
 flag = None
+
+
+def random_shape(n) -> nx.Graph:
+    '''
+    Outputs a random shape as nx.Graph
+
+    ..note:: set `random.seed()` for seeding
+    
+    Args:
+        n (int): Number of shapes in the bank to draw from
+    
+    '''
+    shape_list = [
+        house,
+        pentagon,
+        wheel
+    ]
+    i = random.choice(list(range(len(shape_list))))
+    return shape_list[i], i + 1
--- test_scripts/datasets/BAH/synth_bah/test_BAH_vis.py
+++ test_scripts/datasets/BAH/synth_bah/test_BAH_vis.py
@@ -1,16 +1,34 @@
+import sys
 from graphxai.datasets import BAShapes
+
+assert len(sys.argv) == 4, 'usage: python3 test_BAH_vis.py <shape_insert_strategy> <labeling_method> <feature_method>'
+
+assert sys.argv[1] in ['ub', 'lb', 'global'], "shape_insert_strategy must be in ['ub', 'lb', 'global']"
+assert sys.argv[2] in ['e', 'f', 'ef'], "labeling_method must be in ['e', 'f', 'ef']"
+assert sys.argv[3] in ['gaussian', 'ns', 'onehot'], "feature_method must be in ['gaussian', 'ns', 'onehot']"
+
+pm_conv = {'ub':'neighborhood upper bound', 'lb':'local', 'global':'global'}
+lm_conv = {'e':'edge', 'f':'feature', 'ef':'edge and feature'}
+fm_conv = {'gaussian':'gaussian', 'ns':'network stats', 'onehot':'onehot'}
+
+if sys.argv[1] == 'global':
+    num_shapes = 5
+elif sys.argv[1] == 'lb':
+    num_shapes = 1
+elif sys.argv[1] == 'ub':
+    num_shapes = None
 
 class Hyperparameters:
     num_hops = 2
-    n = 50
+    n = 100
     m = 1
-    num_shapes = None
-    plant_method = 'neighborhood upper bound'
+    num_shapes = num_shapes
+    shape_insert_strategy = pm_conv[sys.argv[1]]
     shape_upper_bound = 1
-    labeling_method = 'edge'
+    labeling_method = lm_conv[sys.argv[2]]
 
 hyp = Hyperparameters
 args = {key:value for key, value in hyp.__dict__.items() if not key.startswith('__') and not callable(value)}
-bah = BAShapes(**args, feature_method = 'onehot')
+bah = BAShapes(**args, feature_method = fm_conv[sys.argv[3]], shape = 'random')
 
-bah.visualize()
+bah.visualize(shape_label = False)
