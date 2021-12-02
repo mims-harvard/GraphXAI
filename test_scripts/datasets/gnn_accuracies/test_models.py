1a9417abd5a426155fb8aedd22c79804269083ed
Owen Queen <owencqueen@hotmail.com>
2021-12-01 23:26:54 -0500

    From macbook

---
+++ test_scripts/datasets/gnn_accuracies/test_models.py
@@ -0,0 +1,114 @@
+import torch
+import numpy as np
+import scipy.stats as st
+
+from tqdm import trange
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+
+from graphxai.datasets.shape_graph import ShapeGraph
+from graphxai.gnn_models.node_classification.testing import GCN_3layer_basic, GIN_3layer_basic
+from graphxai.gnn_models.node_classification.testing import GCN_2layer, GIN_2layer
+
+def train_on_split(
+        model, 
+        optimizer,
+        criterion, 
+        data,
+        split
+    ):
+
+    model.train()
+    optimizer.zero_grad()  # Clear gradients.
+    out = model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = criterion(out[split], data.y[split])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    optimizer.step()  # Update parameters based on gradients.
+    return loss
+
+def test_on_split(
+        model, 
+        data, 
+        split,
+        num_classes = 2
+    ):
+    model.eval()
+    out = model(data.x, data.edge_index)
+    pred = out.argmax(dim=1)  # Use the class with highest probability.
+
+    acc = accuracy_score(data.y[split].tolist(), pred[split].tolist())
+    if num_classes == 2:
+        test_score = f1_score(data.y[split].tolist(), pred[split].tolist())
+        precision = precision_score(data.y[split].tolist(), pred[split].tolist())
+        recall = recall_score(data.y[split].tolist(), pred[split].tolist())
+        return test_score, acc, precision, recall
+    
+    return acc
+
+def test_model_on_ShapeGraph(model, epochs_per_run = 200, num_cvs = 50):
+
+    # Cross-validate the model 10 times:
+    f1_cv = []
+    acc_cv = []
+    prec_cv = []
+    rec_cv = []
+
+    for i in trange(num_cvs):
+        # Gen dataset:
+        bah = ShapeGraph(model_layers = 3)
+        data = bah.get_graph()
+        # Cross-validation split on dataset nodes:
+        kf = KFold(n_splits = 10, shuffle = True)
+        nodes = list(range(bah.num_nodes))
+
+        f1_cvi = []
+        acc_cvi = []
+        prec_cvi = []
+        rec_cvi = []
+
+        for train_index, test_index in kf.split(nodes):
+
+            # Set optimizer, loss function:
+            modeli = model()
+            optimizer = torch.optim.Adam(modeli.parameters(), lr = 0.01)
+            criterion = torch.nn.CrossEntropyLoss()
+
+            for epoch in range(epochs_per_run):
+                loss = train_on_split(modeli, optimizer, criterion, data, train_index)
+                
+            f1, acc, precision, recall = test_on_split(modeli, data, test_index, num_classes = 2)
+
+            f1_cvi.append(f1); acc_cvi.append(acc); prec_cvi.append(precision); rec_cvi.append(recall)
+
+        # Append all means:
+        f1_cv.append(np.mean(f1_cvi))
+        acc_cv.append(np.mean(acc_cvi))
+        prec_cv.append(np.mean(prec_cvi))
+        rec_cv.append(np.mean(rec_cvi))
+
+    metrics = ['F1', 'Accuracy', 'Precision', 'Recall']
+    results = [f1_cv, acc_cv, prec_cv, rec_cv]
+    for i in range(4):
+        l = results[i]
+
+        # Get confidence interval:
+        ci = st.t.interval(alpha = 0.95, df = len(l) - 1, loc = np.mean(l), scale = st.sem(l))
+
+        print('{} Score: {:.4f} +- {:.4f}'.format(metrics[i], np.mean(l), (ci[1] - ci[0]) / 2))
+
+if __name__ == '__main__':
+    model = lambda : GIN_3layer_basic(
+        hidden_channels=64, 
+        input_feat = 10,
+        classes = 2)
+    print('GIN 3 layer, hc = 64')
+    # model = lambda : GCN_2layer(
+    #     hidden_channels=64, 
+    #     input_feat = 10,
+    #     classes = 2)
+
+    test_model_on_ShapeGraph(model, epochs_per_run=500)
+
+
+
+
--- test_scripts/explainers/test_GBP_BA.py
+++ test_scripts/explainers/test_GBP_BA.py
@@ -4,41 +4,21 @@ import matplotlib.pyplot as plt
 
 from graphxai.explainers import GuidedBP
 from graphxai.explainers.utils.visualizations import visualize_subgraph_explanation
-from graphxai.gnn_models.node_classification import BA_Houses, GCN, train, test
+from graphxai.gnn_models.node_classification import BA_Houses, train, test
+from graphxai.gnn_models.node_classification.testing import GCN_3layer_basic
 from graphxai.datasets import BAHouses, BAShapes
+from graphxai.datasets.shape_graph import ShapeGraph
 
 from graphxai.visualization.explanation_vis import visualize_node_explanation
 
-n = 300
-m = 2
-num_houses = 20
-
-hyp = {
-    'num_hops': 1,
-    'n': n,
-    'm': m,
-    'num_shapes': num_houses,
-    'shape_insert_strategy': 'bound_12',
-    'labeling_method': 'edge',
-    'shape_upper_bound': 1,
-    'feature_method': 'gaussian_lv'
-}
-
-# bah = BAShapes(
-#     num_hops = 2,
-#     n = 2000,
-#     m = 1,
-#     num_shapes = None,
-#     shape_insert_strategy = 'neighborhood upper bound'
-# )
-bah = BAShapes(**hyp)
-#data, inhouse = bah.get_data(num_houses, multiple_features=True)
+# Set up dataset:
+bah = ShapeGraph(model_layers = 3)
 data = bah.get_graph(use_fixed_split=True)
 
 # Get nodes in the house:
-inhouse = (data.y == 0).nonzero(as_tuple=True)[0]
+inhouse = (data.y == 1).nonzero(as_tuple=True)[0]
 
-model = GCN(64, input_feat = 10, classes = 2)
+model = GCN_3layer_basic(64, input_feat = 10, classes = 2)
 print(model)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
@@ -59,20 +39,12 @@ exp = gbp.get_explanation_node(data.x, data.y, edge_index = data.edge_index, nod
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
 ground_truth = bah.explanations[node_idx] # Get Explanation object
-
-#visualize_node_explanation(ground_truth, ax = ax1, show = False)
-ground_truth.context_draw(num_hops = bah.num_hops, additional_hops = 1, heat_by_exp = True, ax=ax1)
+ground_truth.context_draw(num_hops = bah.num_hops, graph_data = data, additional_hops = 1, heat_by_exp = True, ax=ax1)
 ax1.set_title('Ground Truth')
-
-# Use first dimension of explanation matrix as the displayed explanation value:
-# visualize_subgraph_explanation(exp.enc_subgraph.edge_index, 
-#     [exp.node_imp[i,0].item() for i in range(exp.node_imp.shape[0])], 
-#     node_idx = int(node_idx), ax = ax2, show = False)
 
 # Aggregate node_imp for visualization:
 exp.node_imp = [torch.sum(exp.node_imp[i]).item() for i in range(len(exp.node_imp))]
-
-exp.context_draw(num_hops = bah.num_hops, additional_hops = 1, heat_by_exp = True, ax=ax2)
+exp.context_draw(num_hops = bah.num_hops, graph_data = data, additional_hops = 1, heat_by_exp = True, ax=ax2)
 ax2.set_title('Guided Backprop (Explanation wrt Degree)')
 
 model.eval()
@@ -82,5 +54,4 @@ ymin, ymax = ax1.get_ylim()
 xmin, xmax = ax1.get_xlim()
 ax1.text(xmin, ymax - 0.1*(ymax-ymin), 'Label = {:d}'.format(data.y[node_idx].item()))
 ax1.text(xmin, ymax - 0.15*(ymax-ymin), 'Pred  = {:d}'.format(pred.argmax(dim=0).item()))
-
 plt.show()
