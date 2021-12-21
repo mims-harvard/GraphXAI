8eacf01ce6e5318941d4417e094302ec7592e72b
owencqueen <owencqueen@hotmail.com>
2021-12-21 01:15:55 -0500

    Updates to documentation, house testing scripts

--- graphxai/gnn_ex_eval/explainers/__init__ 2.py
+++
@@ -1,7 +0,0 @@
-from .grad import VanillaGrad
-from .ig import IntegratedGrad
-from .graphlime import GLIME
-from .randomfeat import RandomFeatures
-from .randomedge import RandomEdges
-from .graphmask import GraphMask
-from .gnnexplainer import GNNExplainer
