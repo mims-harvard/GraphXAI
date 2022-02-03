f99d9dca3d730a290e086d6958a0386d3bc5e008
owencqueen <owencqueen@hotmail.com>
2022-02-03 16:24:27 -0500

    Towards optimizing - no testing done yet

--- formal/metrics.py
+++ formal/metrics.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from numpy import ndarray
 from torch_geometric.utils import to_networkx
+from torch_geometric.data import Data
 
 from graphxai.explainers import CAM, GradCAM, GNNExplainer
 # from graphxai.explainers.utils.visualizations import visualize_subgraph_explanation
@@ -288,12 +289,18 @@ def graph_exp_stability(generated_exp: Explanation, explainer, shape_graph: Shap
     Y = data.y.to(device)
     EIDX = data.edge_index.to(device)
 
+    G = to_networkx_conv(data) # Cache graph to use in rewire_edges
+    data_for_rewire = Data(edge_index = EIDX, num_nodes = 1)
+
     for run in range(num_run):
         # Generate perturbed counterpart
-        pert_edge_index = rewire_edges(EIDX, node_idx=node_id, num_nodes=1).to(device)  # , seed=run)
+        #pert_edge_index = rewire_edges(EIDX, node_idx=node_id, num_nodes=1).to(device)  # , seed=run)
         try:
             # import time; st_time = time.time()
-            pert_edge_index = rewire_edges(EIDX, node_idx=node_id, num_nodes=1).to(device)  # , seed=run)
+            pert_edge_index = rewire_edges(EIDX, 
+                data = data_for_rewire,
+                G = G,
+                node_idx=node_id, num_nodes=1).to(device)  # , seed=run)
             # print(time.time()-st_time)
         except:
             continue
