d0662ccebe0a131f6f464cba17ea0439e1c19b89
Queen <owq978@compute-g-16-254.o2.rc.hms.harvard.edu>
2022-02-04 01:57:27 -0500

    from cluster

--- formal/ShapeGraph/owen_benchmarks/eval_stab.sh
+++ formal/ShapeGraph/owen_benchmarks/eval_stab.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -c 1
-#SBATCH -t 0-24:00
+#SBATCH -t 0-12:00
 #SBATCH -p gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=10G
-#SBATCH -o eval_PGMEX.out
-#SBATCH -e eval_PGMEX.err
-#SBATCH -J PGMEX
+#SBATCH -o eval_IG.out
+#SBATCH -e eval_IG.err
+#SBATCH -J IG
 
 source activate GXAI
-python3 eval_SG_stability.py --exp_method PGMEX --model GIN --model_path /home/owq978/GraphXAI/formal/model_homophily.pth 
+python3 eval_SG_stability.py --exp_method IG --model GIN --model_path /home/owq978/GraphXAI/formal/model_homophily.pth 
