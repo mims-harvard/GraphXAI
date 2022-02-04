f6ef995ff77e850967f36b93184ef109e3148381
owencqueen <owencqueen@hotmail.com>
2022-02-04 02:07:08 -0500

    to ndarray

--- formal/metrics.py
+++ formal/metrics.py
@@ -464,7 +464,7 @@ def stat_parity(org, pred, sens):
     idx_s0 = np.array(sens) == 0
     idx_s1 = np.array(sens) == 1
 
-    if (sens.unique().shape[0] == 1):
+    if (len(sens.unique()) == 1):
         return 0
     else:
         parity_1 = abs(sum(org[idx_s0]) / sum(idx_s0) - sum(org[idx_s1])/sum(idx_s1))
