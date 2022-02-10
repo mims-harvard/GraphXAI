fdf4b42b0d05e84f173d3a2345f2c7393a088b9f
owencqueen <owencqueen@hotmail.com>
2022-02-10 00:20:14 -0500

    Tools for varying GNN layer testing

--- formal/train_model_owen.py
+++ formal/train_model_owen.py
@@ -3,6 +3,7 @@ import random
 import torch
 import argparse
 from graphxai.gnn_models.node_classification.testing import GCN_3layer_basic, GIN_3layer_basic, test, train, val
+from graphxai.gnn_models.node_classification.testing import GIN_1layer, GIN_2layer
 from graphxai.datasets.shape_graph import ShapeGraph
 from graphxai.datasets  import load_ShapeGraph
 
@@ -29,8 +30,13 @@ else:
 data = bah.get_graph(use_fixed_split=True)
 inhouse = (data.y == 1).nonzero(as_tuple=True)[0]
 
-# Test on 3-layer basic GCN, 16 hidden dim:
-model = GIN_3layer_basic(16, input_feat = 11, classes = 2)
+if args.num_layers == 1:
+    model = GIN_1layer(input_feat = 11, classes = 2)
+elif args.num_layers == 2:
+    model = GIN_2layer(16, input_feat = 11, classes = 2)
+elif args.num_layers == 3:
+    # Test on 3-layer basic GCN, 16 hidden dim:
+    model = GIN_3layer_basic(16, input_feat = 11, classes = 2)
 
 # Train the model:
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -42,7 +48,7 @@ for epoch in range(1, 1001):
     f1, acc, precision, recall, auprc, auroc = val(model, data, get_auc=True)
     if f1 > best_f1:
         best_f1 = f1
-        torch.save(model.state_dict(), f'model_{args.expt_name}.pth')
+        torch.save(model.state_dict(), f'model_{args.expt_name}_L={args.num_layers}.pth')
 
     print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Val F1: {f1:.4f}, Val AUROC: {auroc:.4f}')
 
